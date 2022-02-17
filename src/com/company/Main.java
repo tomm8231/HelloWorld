0d8c16852f3356ffd8e89026586eb1215db0f15c
tomm8231 <tomm8231@stud.kea.dk>
2022-02-17 20:53:21 +0100

    Removed "How's it going"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("\"Hello,\n\t\tcruel world.\n\nHow's it going?\"\n");
+        System.out.println("\"Hello,\n\t\tcruel world.");
     }
 }
